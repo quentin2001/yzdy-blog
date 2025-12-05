60bcfea5af2d5c37e5d68cd12ce66099a0979d93
yzdy <653731317@qq.com>
2025-12-05 19:24:44 +0800

    feat(照片): 更新猫咪照片列表并移除旧照片
    移除不再使用的旧照片文件，并添加新的猫咪照片到照片列表中

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -3,7 +3,11 @@ import cat1 from '~/assets/photos/cats/cat1.webp'
 import cat2 from '~/assets/photos/cats/cat2.webp'
 import cat3 from '~/assets/photos/cats/cat3.webp'
 import cat4 from '~/assets/photos/cats/cat4.webp'
-
+import cat5 from '~/assets/photos/cats/cat5.webp'
+import cat6 from '~/assets/photos/cats/cat6.webp'
+import cat10 from '~/assets/photos/cats/cat10.webp'
+import cat11 from '~/assets/photos/cats/cat11.webp'
+import cat9 from '~/assets/photos/cats/cat9.webp'
 
 export const PhotosList: PhotoData[] = [
   {
@@ -42,6 +46,41 @@ export const PhotosList: PhotoData[] = [
         alt: "My Adorable Cate",
         width: cat4.width,
         height: cat4.height,
+        variant: '4x3',
+      },
+      {
+        src: cat5,
+        alt: "My Adorable Cate",
+        width: cat5.width,
+        height: cat5.height,
+        variant: '4x3',
+      },
+      {
+        src: cat6,
+        alt: "My Adorable Cate",
+        width: cat6.width,
+        height: cat6.height,
+        variant: '4x3',
+      },
+            {
+        src: cat10,
+        alt: "My Adorable Cate",
+        width: cat10.width,
+        height: cat10.height,
+        variant: '4x3',
+      },
+      {
+        src: cat11,
+        alt: "My Adorable Cate",
+        width: cat11.width,
+        height: cat11.height,
+        variant: '4x3',
+      },
+      {
+        src: cat9,
+        alt: "My Adorable Cate",
+        width: cat9.width,
+        height: cat9.height,
         variant: '4x3',
       },
     ],
